28efe9b717372a05bd1cacb74d8a7c02ee2142e0
Rotimi Rotimi <matanmearoja@yahoo.com>
2023-04-14 08:40:29 -0700

    update file

--- css/index.js
+++ css/index.js
@@ -1,13 +1,109 @@
-window.addEventListener('load', () => {
-    todos =JSON.parse(localStorage.getItem('todos')) || [];
-    const nameInput = document.querySelector('#name');
-    const newTodoForm = document.querySelector('#newTodoForm');
+window.addEventListener("load", () => {
+  todos = JSON.parse(localStorage.getItem("todos")) || [];
+  const nameInput = document.querySelector("#name");
+  const newTodoForm = document.querySelector("#newTodoForm");
 
-    const userName = localStorage.getItem('username') || '';
+  const username = localStorage.getItem("username") || "";
 
-    nameInput.value = username;
+  nameInput.value = username;
 
-nameInput.addEventListener('change', e => {
-    localStorage.setItem('username', e.target.value);
-})
-})
+  nameInput.addEventListener("change", (e) => {
+    localStorage.setItem("username", e.target.value);
+  });
+
+  newTodoForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const todo = {
+      content: e.target.elements.content.value,
+      category: e.target.category.value,
+      done: false,
+      createdAt: new Date().getTime(),
+    };
+    todos.push(todo);
+    localStorage.setItem("todo", JSON.stringify(todos));
+    e.target.removeEventListener();
+    DisplayTodos();
+  });
+});
+
+function DisplayTodos() {
+  const todoList = document.querySelector("#todo-list");
+  todoList.innerHTML = "";
+
+  todos.forEach((element) => {
+    const todoItem = document.createElement("div");
+    todoItem.classList.add("todo-item");
+
+    const label = document.createElement("label");
+    const input = document.createElement("input");
+    const span = document.createElement("span");
+    const content = document.createElement("div");
+    const actions = document.createElement("div");
+    const edit = document.createElement("button");
+    const seleteButton = document.createElement("button");
+
+    input.type = "checkbox";
+    input.classList = todo.done;
+    span.classList.add("personal");
+
+    if (todo.category == "personal") {
+      span.classList.add("personal");
+    } else {
+      span.classList.add("business");
+    }
+
+    content.classList.add("todo-content");
+    actions.classList.add("actions");
+    edit.classList.add("edit");
+    deleteButton.classList.add("delete");
+
+    content.innerHTML = `<input type='text' value='${todo.content}' readonly>`;
+    edit.innerHTML = "Edit";
+    deleteButton.innerHTML = "Delete";
+
+    label.appendChild(input);
+    label.appendChild(span);
+    actions.appendChild(edit);
+    label.appendChild(deleteButton);
+    todoItem.appendChild(label);
+    todoItem.appendChild(content);
+    todoItem.appendChild(actions);
+
+    todoList.appendChild(todoItem);
+
+    if (todo.done) {
+      todoItem.classList.add("done");
+    }
+
+    input.addEventListener("click", (e) => {
+      todo.done = e.target.checked;
+      localStorage.setItem("todos", JSON.stringify(todos));
+
+      if (todo.done) {
+        todoItem.classList.add("done");
+      } else {
+        todoItem.classList.remove("done");
+      }
+
+      DisplayTodos();
+    });
+
+    edit.addEventListener("click", (e) => {
+      const input = content.querySelector("input");
+      input.removeAttribute("readonly");
+      input.focus();
+      input.addEventListener("blur", (e) => {
+        input.setAttribute("readonly", true);
+        todo.content = e.target.value;
+        localStorage.setItem("todos", JSON.stringify(todos));
+        DisplayTodos();
+      });
+    });
+
+    deleteButton.addEventListener("click", (e) => {
+      todos = todos.filter((t) => t != todo);
+      localStorage.setItem("todos", JSON.stringify("todo"));
+      DisplayTodos();
+    });
+  });
+}
